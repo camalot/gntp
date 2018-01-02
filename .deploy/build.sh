3af28c887126c57558c79be2442e29159954d4aa
Ryan Conrad <camalot@gmail.com>
2018-01-01 22:11:43 -0600

    dir

--- .deploy/build.sh
+++ .deploy/build.sh
@@ -29,6 +29,6 @@ echo "${opt_project_name}-${opt_version}.zip";
 mkdir -p "${WORKSPACE}/dist/";
 pushd .;
 cd ${WORKSPACE}/root;
-zip -r "${WORKSPACE}/dist/${opt_project_name}-${opt_version}.zip" "${WORKSPACE}/root/usr" > /dev/null;
+zip -r "${WORKSPACE}/dist/${opt_project_name}-${opt_version}.zip" "./*" > /dev/null;
 
 popd;
