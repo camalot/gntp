970bcc8421b9319cd1524ef0c2e7ac469fcf05d7
Ryan Conrad <camalot@gmail.com>
2018-01-07 20:13:09 -0600

    update script

--- root/usr/local/bin/gntp.sh
+++ root/usr/local/bin/gntp.sh
@@ -1,41 +1,54 @@
 #!/usr/bin/env bash
 __error() {
-	(>&2 echo "${1:-"Unknown Error"}");
+	RED='\033[0;31m';
+	NC='\033[0m';
+	dt=$(date "+%F %T");
+	(>&2 echo "${RED}[$dt]\tERROR\t$(basename $0)\t${1:-"Unknown Error"}${NC}");
 	exit 9;
 }
-
-__opts() {
-	while getopts ":t:m:" opt; do
-	  case $opt in
-      t) export opt_title="$OPTARG";
-      ;;
-      m) export opt_message="$OPTARG";
-      ;;
-    	\?) 
-				echo "Invalid option -$OPTARG" >&2;
-	    	exit 1;
-	    ;;
-	  esac;
-	done;
-
-	return 0;
-};
+__warning() {
+	YELLOW='\033[0;33m';
+	NC='\033[0m';
+	dt=$(date "+%F %T");
+	(>&2 echo "${YELLOW}[$dt]\WARNING\t$(basename $0)\t${1:-"Unknown Warning"}${NC}");
+}
 
 __gntp() {
-    [[ -p "${GNTP_PASSWORD// }" ]] && __error "Environment Variable 'GNTP_PASSWORD' was not defined";
-    [[ -p "${GNTP_HOST// }" ]] && __error "Environment Variable 'GNTP_HOST' was not defined";
-    [[ -p "${GNTP_APP// }" ]] && __error "Environment Variable 'GNTP_HOST' was not defined";
+	for i in "$@"; do
+		case $i in
+			-a=*|--app=*)
+				_opt_app="${i#*=}";
+				shift # past argument=value
+			;;
+			-t=*|--title=*)
+				_opt_title="${i#*=}";
+				shift # past argument=value
+			;;
+			-m=*|--message=*)
+				_opt_message="${i#*=}";
+				shift # past argument=value
+			;;
+			*)
+				__error "Unknown option '$i'";
+			;;
+		esac
+	done
+	app_name="${_opt_app:-"${GNTP_APP}"}";
+	[[ -z "${GNTP_PASSWORD// }" ]] && __error "Environment Variable 'GNTP_PASSWORD' was not defined";
+	[[ -z "${GNTP_HOST// }" ]] && __error "Environment Variable 'GNTP_HOST' was not defined";
 
-    __opts $@;
+	[[ -z "${_opt_title// }" ]] && __error "Missing required title argument (-t|--title)";
+	[[ -z "${app_name// }" ]] && __error "Missing required title argument (-a|--app)";
+	[[ -z "${_opt_message// }" ]] && __error "Missing required message argument (-m|--message)";
 
-    # gntp-send: [-u] [-i] [-a APPNAME] [-n NOTIFY] [-s SERVER:PORT] [-p PASSWORD] title message [icon] [url]
-    echo "gntp-send -s \"${GNTP_HOST}\" -a \"${GNTP_APP}\" -p \"*******************\" \"$opt_title\" \"$opt_message\"";
-    gntp-send \
-    -s "${GNTP_HOST}" \
-    -a "${GNTP_APP:-"${JENKINS_CLOUD_ID}-${DOCKER_CONTAINER_ID}"}" \
-    -p "${GNTP_PASSWORD}" \
-    "$opt_title" \
-    "$opt_message";
+	# gntp-send: [-u] [-i] [-a APPNAME] [-n NOTIFY] [-s SERVER:PORT] [-p PASSWORD] title message [icon] [url]
+	echo "gntp-send -s \"${GNTP_HOST}\" -a \"${GNTP_APP}\" -p \"*******************\" \"$_opt_title\" \"$_opt_message\"";
+	gntp-send \
+	-s "${GNTP_HOST}" \
+	-a "${GNTP_APP:-"${JENKINS_CLOUD_ID}-${DOCKER_CONTAINER_ID}"}" \
+	-p "${GNTP_PASSWORD}" \
+	"$_opt_title" \
+	"$_opt_message";
 }
 
 __gntp $@;
