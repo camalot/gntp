563c2de1f3107ac1c6531f36a726fbaead8f9d4a
Ryan Conrad <camalot@gmail.com>
2018-01-07 21:04:51 -0600

    update output

--- root/usr/local/bin/gntp.sh
+++ root/usr/local/bin/gntp.sh
@@ -3,14 +3,14 @@ __error() {
 	RED='\033[0;31m';
 	NC='\033[0m';
 	dt=$(date "+%F %T");
-	(>&2 echo "${RED}[$dt]\tERROR\t$(basename $0)\t${1:-"Unknown Error"}${NC}");
+	(>&2 echo -e "${RED}[$dt]\tERROR\t$(basename $0)\t${1:-"Unknown Error"}${NC}");
 	exit 9;
 }
 __warning() {
 	YELLOW='\033[0;33m';
 	NC='\033[0m';
 	dt=$(date "+%F %T");
-	(>&2 echo "${YELLOW}[$dt]\WARNING\t$(basename $0)\t${1:-"Unknown Warning"}${NC}");
+	(>&2 echo -e "${YELLOW}[$dt]\WARNING\t$(basename $0)\t${1:-"Unknown Warning"}${NC}");
 }
 
 __gntp() {
@@ -18,15 +18,15 @@ __gntp() {
 		case $i in
 			-a=*|--app=*)
 				_opt_app="${i#*=}";
-				shift # past argument=value
+				shift; # past argument=value
 			;;
 			-t=*|--title=*)
 				_opt_title="${i#*=}";
-				shift # past argument=value
+				shift; # past argument=value
 			;;
 			-m=*|--message=*)
 				_opt_message="${i#*=}";
-				shift # past argument=value
+				shift; # past argument=value
 			;;
 			*)
 				__error "Unknown option '$i'";
